4d7c09c8165184aa2979e875c3490e28280c9075
FaaDiiL <fadil.alfarhan@gmail.com>
2023-06-14 20:33:39 +0200

    feat: Add student registration endpoint and service method
    This commit introduces a new POST endpoint at "api/v1/student" that enables the registration of a new student. The registerNewStudent method in the controller receives a Student object through the request body and delegates the registration process to the addNewStudent method in the studentService class. The addNewStudent method, in turn, simply prints the student details to the console.

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -1,9 +1,7 @@
 package com.example.demo.student;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,5 +18,10 @@ public class StudentController {
     @GetMapping(path = "api/v1/student")
     public List<Student> getStudents(){
         return studentService.getStudents();
+    }
+
+    @PostMapping("api/v1/student")
+    public void registerNewStudent(@RequestBody Student student) {
+        studentService.addNewStudent(student);
     }
 }
