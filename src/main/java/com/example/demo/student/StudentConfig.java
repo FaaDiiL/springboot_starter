fe2841d1386034b564f2bc963046e646abad3b7b
FaaDiiL <fadil.alfarhan@gmail.com>
2023-06-14 03:03:22 +0200

    Refactor code, add application properties uncomment dependency in pom.xml to start use postgres database add StudentRepository add student config to insert som default students to database

---
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -0,0 +1,44 @@
+package com.example.demo.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner (StudentRepository repository) {
+        return args -> {
+
+            Student kayzer = new Student(
+                    "Kayzer",
+                    "Superti@outlook.com",
+                    LocalDate.of(1995, Month.JANUARY, 1),
+                    28
+            );
+
+            Student robert = new Student(
+                    "Robert",
+                    "rob@gmail.com",
+                    LocalDate.of(1990, Month.JANUARY, 1),
+                    33
+            );
+
+            Student eyad = new Student(
+                    "Eyad",
+                    "Eyad@hotmail.com",
+                    LocalDate.of(1985, Month.JANUARY, 1),
+                    38
+            );
+
+            repository.saveAll(
+                    List.of(kayzer,robert,eyad)
+            );
+        };
+    }
+}
